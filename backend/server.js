9beb567ab99c1f2ba4c4cd828a23228a378bb151
Matthew Shao <matthewshao@gmail.com>
2026-04-06 15:59:04 -0500

    fixing PORT

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const pool = require('./db');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
